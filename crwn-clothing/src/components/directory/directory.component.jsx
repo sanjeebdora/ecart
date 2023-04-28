ae7d31b002be5dd9bb3b9ec14d415eba766b2cc0
Sanjeeb Dora <sanjeebdora@Sanjeebs-Mac-mini.local>
2023-04-28 15:13:12 +0530

    Product Card Component Created

--- crwn-clothing/src/components/directory/directory.component.jsx
+++ crwn-clothing/src/components/directory/directory.component.jsx
@@ -1,11 +1,15 @@
-import CategoryItem from '../category-item/category-item.componet';
+import CategoryItem from '../category-item/category-item.component';
+
 import './directory.styles.scss';
 
-const Directory = ({ categories }) => (
-  <div className="directory-container">
-    {categories.map((category) => (
-      <CategoryItem category={category} key={category.id} />
-    ))}
-  </div>
-);
+const Directory = ({ categories }) => {
+  return (
+    <div className='directory-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
 export default Directory;
